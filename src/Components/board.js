527971aa5ae20c0e415713d8766c35b35904223f
soca-git <soca.mailbox@gmail.com>
2022-09-21 22:56:57 +0100

    css updates

--- src/Components/board.js
+++ src/Components/board.js
@@ -52,24 +52,26 @@ class Board extends React.Component {
         const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
-        <div>
+        <>
             <div className="status">{status}</div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            <div className="board">
+                <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-        </div>
+        </>
         );
     }
 }
