9b730427568e505ebcc3d3e3b56c086999fb0ee0
soca-git <soca.mailbox@gmail.com>
2022-09-21 22:28:13 +0100

    progressed tutorial until 'Declaring a Winner'

--- src/Components/board.js
+++ src/Components/board.js
@@ -2,12 +2,54 @@ import React from "react";
 import Square from "./square";
 
 class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // in js classes, always need to call the parent (super) class constructor.
+
+        console.log(props);
+        // props is saved as a property of this object when the object is initialized.
+
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        }
+        // uplift the square's state into the parent(board) component.
+    }
+
+    logClick(i) {
+        console.log(`clicked square ${i}!`);
+    }
+
+    whenClicked(i) {
+        if (this.state.squares[i] != null)
+        {
+            return;
+        }
+        // prevent the same square being set multiple times.
+
+        this.logClick(i);
+
+        const squaresCopy = this.state.squares.slice();
+        // By copying the square's state into a new object, we are making changes "immutable".
+        // This makes otherwise complex features easier to implement, like storing the game's history,
+        // since detecting changes between immutable objects is considerably easier than mutable ones.
+        // Comparing mutable objects (where we change the object's data directly) is much more involved,
+        // requiring traversal of the object's tree and comparison to previous copies of itself.
+
+        squaresCopy[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squaresCopy,
+            xIsNext: !this.state.xIsNext
+        }); 
+    }
+
     renderSquare(i) {
-        return <Square number={i} />;
+        return <Square number={i} value={this.state.squares[i]} onClick= {() => this.whenClicked(i)} />;
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
         <div>
--- src/Components/square.js
+++ src/Components/square.js
@@ -1,37 +1,43 @@
 import React from "react";
 
-class Square extends React.Component {
+// class Square extends React.Component {
     
-    constructor(props) {
-        super(props);
-        // in js classes, always need to call the parent (super) class constructor.
-
-        console.log(props);
-        // props is saved as a property of this object when the object is initialized.
+//     constructor(props) {
+//         super(props);
+//         // in js classes, always need to call the parent (super) class constructor.
         
-        this.state = {
-            value: null
-        };
-        // state represents the memory of a component, i.e. how it can "remember" things.
-    }
+//         this.state = {
+//             value: null
+//         };
+//         // state represents the memory of a component, i.e. how it can "remember" things.
+//         // note: we aren't using the square's state for anything, we uplifted it's state management to the board component,
+//         // and pass it down through the props.
+//     }
 
-    whenClicked()
-    {
-        this.logClick(this.props.number);
-        this.setState({value: 'X'});
-    }
+//     whenClicked()
+//     {
+//         console.log("do something else maybe...");
+//         this.props.onClick();
+//     }
     
-    logClick(i) {
-        console.log(`clicked square ${i}!`);
-    }
+//     render() {
+//         return (
+//         <button className="square" onClick={() => this.whenClicked()}>
+//             {this.props.value}
+//         </button>
+//         );
+//     }
+// }
 
-    render() {
-        return (
-        <button className="square" onClick={() => this.whenClicked()}>
-            {this.state.value}
+// In React, function components are a simpler way to write components that only contain a render method and don’t have their own state.
+// Instead of defining a class which extends React.Component, we can write a function that takes props as input and returns what should be rendered.
+// Function components are less tedious to write than classes, and many components can be expressed this way.
+function Square(props) {
+    return (
+        <button className="square" onClick={() => props.onClick()}>
+            {props.value}
         </button>
-        );
-    }
+    );
 }
 
 export default Square;
