b4ca0e19c287f8eab2f70a91418cb88e2c27c5d6
soca-git <soca.mailbox@gmail.com>
2022-09-21 20:00:28 +0100

    add game template files

---
+++ src/Components/square.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+}
+
+export default Square;
